487f59b5108fe0af984c0b0681ab421a9775b946
Bruno Tognoli <bruno.tognoli@aluno.senai.br>
2025-11-27 16:23:05 -0300

    Finalização, do dia 1 -_-.

---
+++ Alboz/html_alboz/cadastro_produtos.php
@@ -0,0 +1,14 @@
+<?php
+include('protecao.php');
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastro de Produtos</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ Alboz/html_alboz/meus_produtos.php
@@ -0,0 +1,14 @@
+<?php
+include('protecao.php');
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Meus produtos</title>
+</head>
+<body>
+
+</body>
+</html>
